c4afc0e481328aa6e0c1e19c13e5f26fa97f2779
fganga <fredy.ganga@gmail.com>
2014-09-24 03:50:20 -0300

    Acceso a base de datos
    Confirmación Inicial

---
+++ WpfApplication2/WpfApplication2/NominaCarreras.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Lógica de interacción para NominaCarreras.xaml
+    /// </summary>
+    public partial class NominaCarreras : Window
+    {
+        public NominaCarreras()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WpfApplication2.bd_inacapDataSet bd_inacapDataSet = ((WpfApplication2.bd_inacapDataSet)(this.FindResource("bd_inacapDataSet")));
+            // Cargar datos en la tabla carreras. Puede modificar este código según sea necesario.
+            WpfApplication2.bd_inacapDataSetTableAdapters.carrerasTableAdapter bd_inacapDataSetcarrerasTableAdapter = new WpfApplication2.bd_inacapDataSetTableAdapters.carrerasTableAdapter();
+            bd_inacapDataSetcarrerasTableAdapter.Fill(bd_inacapDataSet.carreras);
+            System.Windows.Data.CollectionViewSource carrerasViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("carrerasViewSource")));
+            carrerasViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void carrerasDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2/NuevaCarrera.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Lógica de interacción para NuevaCarrera.xaml
+    /// </summary>
+    public partial class NuevaCarrera : Window
+    {
+        public NuevaCarrera()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_guardar_Click(object sender, RoutedEventArgs e)
+        {
+            string cadenaConexion           = ConfigurationManager.ConnectionStrings["WpfApplication2.Properties.Settings.bd_inacapConnectionString"].ToString();
+            SqlConnection conexionBdInacap  = new SqlConnection(cadenaConexion);
+
+            SqlCommand insertaCarrera       = new SqlCommand("insert into carreras(id_carrera,nombre_carrera,tipo_carrera) values(@v_id,@v_nombre,@v_tipo)", conexionBdInacap);
+           
+            insertaCarrera.Parameters.AddWithValue("@v_id", txt_codigo.Text.ToString());
+            insertaCarrera.Parameters.AddWithValue("@v_nombre", txt_nombre.Text.ToString());
+            insertaCarrera.Parameters.AddWithValue("@v_tipo", txt_tipo.Text.ToString());
+
+            try
+            {
+                conexionBdInacap.Open();
+                insertaCarrera.ExecuteNonQuery();
+                lbl_resultado.Content = "Nueva Carrera registrada correctamente.";
+            }
+            catch (Exception)
+            {
+
+                lbl_resultado.Content = "Error";
+            }
+
+
+        }
+    }
+}
